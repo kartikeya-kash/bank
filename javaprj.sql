bbe14f5bb4ef899ac4cc458c47ee46580ced3fca
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-11 11:52:44 +0530

    created user table

--- javaprj.sql
+++ javaprj.sql
@@ -1,1 +1,12 @@
-create database javaprj;
+create database javaprj; -- created a database
+use javaprj; -- using the database
+
+create table user (
+    user_id varchar(20) not null primary key,
+    name varchar(60) not null,
+    email varchar(256) not null unique,
+    dob date not null,
+    pass varchar(16) not null,
+    phone bigint not null unique,
+    address varchar(256) not null
+);
