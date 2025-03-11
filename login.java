8f1f6e392243cb05512df2a598c3a103c2208331
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-11 13:56:56 +0530

    login and functions

--- login.java
+++ login.java
@@ -1,3 +1,18 @@
 public class login {
-    
+    private String id ;
+    private String password;
+
+    login (String id , String password){
+        this.id=id;
+        this.password=password;
+    }
+
+    boolean checkCredential(){
+     if(this.id == "" && this.password == ""){
+        return true;
+     }   
+     else{
+        return false;
+     }
+    }
 }
