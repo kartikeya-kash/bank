72c8ec1633218b036cb0d3e879cc8be9cc16c076
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-24 23:08:56 +0530

    changed the bg color

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
+//see photo for color codes
 import java.awt.Color;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Login");
         frame.setLayout(null);
-        frame.setBackground(Color.BLACK);
+        frame.getContentPane().setBackground(new Color(32,87,129)); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setResizable(false);
         frame.setSize(600,500);
         frame.setVisible(true);
@@ -16,10 +17,11 @@ public class Main {
         JLabel label = new JLabel();
         label.setText("Bank");
         label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(50, 200, 150, 30); 
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
-        panel.setBackground(new Color(75,75,75));
+        panel.setBackground(new Color(152,210,192));
         panel.setBounds(0,0,250,500);
         frame.add(panel);
         panel.add(label);
